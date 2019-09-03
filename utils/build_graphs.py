9bf5e1190d6f53fdca809c2176ad3aa9a12da2fe
thiippal <tuomo.hiippala@iki.fi>
2019-09-03 11:31:57 +0300

    fix path join

--- utils/build_graphs.py
+++ utils/build_graphs.py
@@ -2,11 +2,13 @@ import networkx as nx
 import argparse
 import json
 import glob
+import os
 
 
 def json_to_nx(json_f):
     """"
-    A convenience function for creating NetworkX graphs from AI2D-RST JSON annotation.
+    A convenience function for creating NetworkX graphs from AI2D-RST JSON
+    annotation.
 
     Parameters:
         json_f: A JSON file containing AI2D-RST annotation. 
@@ -14,34 +16,40 @@ def json_to_nx(json_f):
     Returns:
         Three NetworkX graphs, one for each annotation layer.
     """
-    
+
     # Read the JSON file
     with open(json_f) as json_file:
-        
+
         # Assign the result into a dictionary
         data = json.load(json_file)
 
-	# Fetch the annotation from the dictionary and assign to variables
+    # Fetch the annotation from the dictionary and assign to variables
     json_grouping = data['grouping']
     json_conn = data['connectivity']
     json_rst = data['rst']
 
-	# Use the NetworkX function for JIT JSON to create the grouping graph 
+    # Use the NetworkX function for JIT JSON to create the grouping graph
     grouping_layer = nx.jit_graph(json_grouping, create_using=nx.Graph())
 
-	# Create the connectivity layer manually
-    connectivity_layer = nx.MultiDiGraph()
+    # Check if connectivity annotation exists
+    if json_conn is not None:
 
-    nodes = json_conn['nodes']
-    edges = json_conn['edges']
+        # Create the connectivity layer manually
+        connectivity_layer = nx.MultiDiGraph()
 
-    for node in nodes:
-        connectivity_layer.add_node(node[0], kind=node[1]['kind'])
+        nodes = json_conn['nodes']
+        edges = json_conn['edges']
 
-    for edge in edges:
-        connectivity_layer.add_edge(edge[0], edge[1], kind=edge[2]['kind'])
+        for node in nodes:
+            connectivity_layer.add_node(node[0], kind=node[1]['kind'])
 
-	# Use the NetworkX function for JIT JSON to create the RST graph 
+        for edge in edges:
+            connectivity_layer.add_edge(edge[0], edge[1], kind=edge[2]['kind'])
+
+    else:
+        connectivity_layer = None
+
+    # Use the NetworkX function for JIT JSON to create the RST graph
     rst_layer = nx.readwrite.jit_graph(json_rst, create_using=nx.DiGraph())
 
     return grouping_layer, connectivity_layer, rst_layer
@@ -58,9 +66,8 @@ if __name__ == '__main__':
     args = vars(ap.parse_args())
     input_dir = args['input']
 
-    for f in glob.glob(input_dir + '*.json'):
-        
+    for f in glob.glob(os.path.join(input_dir, '*.json')):
+
         print("[INFO] Now processing file {} ...".format(f))
 
         grouping_layer, connectivity_layer, rst_layer = json_to_nx(f)
-        
